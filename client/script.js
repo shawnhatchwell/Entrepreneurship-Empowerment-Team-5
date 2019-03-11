a04c5239ca245e0ecde6f60c09c19b0834998dc4
Will Cosker <wcosker@ufl.edu>
2019-03-11 11:51:25 -0400

    added working google calendar

--- client/script.js
+++ client/script.js
@@ -1,11 +1,23 @@
+function onPageLoad(googleUser){
+  try {
+      var profile=googleUser.getBasicProfile();
+}
+catch(e) {
+  console.log("this is null my man");
+}
+}
+window.onload=onPageLoad;
+
 function onSignIn(googleUser){
   var profile=googleUser.getBasicProfile();
   $(".g-signin2").css("display","none");
   $(".data").css("display","block");
+  console.log(profile.getEmail());
   $("#pic").attr('src',profile.getImageUrl());
   $("#email").text(profile.getEmail());
 }
-function signOut(){
+
+function signOut(googleUser){
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function(){
     alert("You have been signed out :)");
