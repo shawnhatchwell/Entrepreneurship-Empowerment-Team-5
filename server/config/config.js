f3ad41844c2dd118810b43f2b2b25dcad0326ed2
Shawn Hatchwell <shawn.hatchwell@ufl.edu>
2019-03-31 16:05:53 -0400

    Added optional port 8080 so remote testing is easier

--- server/config/config.js
+++ server/config/config.js
@@ -5,5 +5,5 @@ module.exports = {
   db: {
     uri: 'mongodb://willcosker:dbpass123@ds041992.mlab.com:41992/bootcamp3wcosker', //place the URI of your mongo database here.
   },
-  port: process.env.PORT
+  port: 8080 || process.env.PORT
 };
