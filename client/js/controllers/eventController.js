58fcf2b84b8fb3c07868c192c0c4b7cac283771e
chaofu30 <stefanpessolano@ufl.edu>
2019-04-08 22:39:34 -0400

    event db and email db

---
+++ client/js/controllers/eventController.js
@@ -0,0 +1,38 @@
+angular.module('events').controller('EventsController', ['$scope', 'Events',
+  function($scope, Events) {
+    /* Get all the events, then bind it to the scope */
+    Events.getAll().then(function(response) {
+      $scope.events = response.data;
+    }, function(error) {
+      console.log('Unable to retrieve events:', error);
+    });
+
+    $scope.detailedInfo = undefined;
+
+    $scope.addEvent = function() {
+	  /**TODO
+	  *Save the article using the Events factory. If the object is successfully
+	  saved redirect back to the list page. Otherwise, display the error
+	 */
+	 /*Make object, push, create*/
+		$scope.events.push($scope.newEvent);
+		Events.create($scope.newEvent);
+    alert($scope.newEvent.emailaddress + " has been added")
+    };
+
+    $scope.deleteEvent = function(id, index) {
+	   /**TODO
+        Delete the article using the Events factory. If the removal is successful,
+		navigate back to 'event.list'. Otherwise, display the error.
+       */
+	   	if (confirm('Are you sure you want to delete "' + $scope.events[index].name + '"?')) {
+			$scope.events.splice(index,1);
+			Events.delete(id);
+		}
+    };
+
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = $scope.events[index];
+    };
+  }
+]);
